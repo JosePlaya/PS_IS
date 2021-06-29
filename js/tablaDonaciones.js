db0bf76709cbe7e03ebc6b1cd2cb7fd47aa87f7f
Jose Playa <jlarenas.s@gmail.com>
2021-06-29 16:43:54 -0400

    Tablas, imagenes

---
+++ js/tablaDonaciones.js
@@ -0,0 +1,50 @@
+//TABLA STOCK INSUMOS
+//Controlador tabla trabajador
+function generarTablaStock(){
+    console.log("INICIANDO PROCESO DE CRACIÓN DE TABLA...")
+    //Obtener el número de trabajadores
+    var nTrabajadores = 4;
+    //Obtener la instancia de la tabla donde se incertaran los datos
+    var contenderoTabla = document.getElementById("contenedorTablaDonaciones")
+
+    //Iniciar el contenedor vacio
+    contenderoTabla.innerHTML = ""
+    
+    //Variable que irá almacenando los valores para la tabla
+    var tabla = "<table>";
+
+    //Crear tabla según la cantidad de filas
+    for (var i=0; i< nTrabajadores; i++){
+        tabla += "<tr>";
+
+        //Tiene que haber un <td> por cada columna creada
+        //Columna 1: Nombre donante
+        tabla += "<td>";
+        tabla += "Juan Antonio Anonimo "+(i+1);
+        //tabla += "<a href='detalleTrabajo.html'><img src='https://picsum.photos/100/100' alt='foto atencion'></a>";
+        tabla += "</td>";
+
+        //Columna 3: Monto
+        tabla += "<td>";
+        tabla += "$"+(i+1)*6520;
+        tabla += "</td>";
+
+        //Columna 5: Fecha
+        tabla += "<td>";
+        tabla += "1"+i+"/0"+(i+1)+"/2018";
+        tabla += "</td>";
+
+        tabla += "</tr>";
+    }
+    //Cerrar el almacenado de los valores para completar la tabla
+    tabla += "</table>";
+
+    //Asignar 'tabla' al contenedor
+    contenderoTabla.innerHTML = tabla;
+
+    console.log("PROCESO FINALIZADO")
+}
+
+
+//Carga los datos automáticamente al iniciar la vista
+window.onload = generarTablaStock();
---
+++ js/tablaVisitantes.js
@@ -0,0 +1,55 @@
+//TABLA STOCK INSUMOS
+//Controlador tabla trabajador
+function generarTablaStock(){
+    console.log("INICIANDO PROCESO DE CRACIÓN DE TABLA...")
+    //Obtener el número de trabajadores
+    var nTrabajadores = 8;
+    //Obtener la instancia de la tabla donde se incertaran los datos
+    var contenderoTabla = document.getElementById("contenedorTablaVisitantes")
+
+    //Iniciar el contenedor vacio
+    contenderoTabla.innerHTML = ""
+    
+    //Variable que irá almacenando los valores para la tabla
+    var tabla = "<table>";
+
+    //Crear tabla según la cantidad de filas
+    for (var i=0; i< nTrabajadores; i++){
+        tabla += "<tr>";
+
+        //Tiene que haber un <td> por cada columna creada
+        //Columna 1: Nombre visitante
+        tabla += "<td>";
+        tabla += "Visitante "+(i+1);
+        //tabla += "<a href='detalleTrabajo.html'><img src='https://picsum.photos/100/100' alt='foto atencion'></a>";
+        tabla += "</td>";
+
+        //Columna 2: Nombre paciente
+        tabla += "<td>";
+        tabla += "Paciente "+(i+1);
+        tabla += "</td>";
+
+        //Columna 3: Descripcion visita
+        tabla += "<td>";
+        tabla += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse accumsan arcu non egestas dapibus. Nunc dolor leo, imperdiet in dapibus sodales, vehicula vitae sem. Nullam eleifend sed nisi feugiat tincidunt. Maecenas a neque sit amet elit ornare varius et quis turpis. Nunc sagittis, dolor et euismod tincidunt, erat ligula lacinia.";
+        tabla += "</td>";
+
+        //Columna 4: Fecha
+        tabla += "<td>";
+        tabla += "1"+i+"/0"+(i+1)+"/2018";
+        tabla += "</td>";
+
+        tabla += "</tr>";
+    }
+    //Cerrar el almacenado de los valores para completar la tabla
+    tabla += "</table>";
+
+    //Asignar 'tabla' al contenedor
+    contenderoTabla.innerHTML = tabla;
+
+    console.log("PROCESO FINALIZADO")
+}
+
+
+//Carga los datos automáticamente al iniciar la vista
+window.onload = generarTablaStock();
